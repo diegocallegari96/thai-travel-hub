868139ef22c6d303b262e68da244201fd2c625bd
diegocallegari96 <warsindustry@gmail.com>
2024-01-17 16:53:01 +0700

    posts and postwidget

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,17 @@
 import Head from "next/head"
 import { PostCard, Categories, PostWidget } from '../components'
+import { getPosts } from '../services'
 
-const posts = [
-  { title: 'Exploring Bang Krachao', excerpt: 'Green lung'},
-  { title: 'Exploring Yaowarat road', excerpt: 'china town'},
-]
-
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <div className='container mx-auto px-10 mb-8'>
       <Head>
         <title>Thai Travel Hub</title>
-        <link rel='icon' href='/thaihubicogeel.svg'/>
+        <link rel='icon' href='/thaihubicowit.svg'/>
       </Head>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
-          {posts.map((post, index) => <PostCard post={post} key={post.title}/>)}
+          {posts.map((post) => <PostCard post={post.node} key={post.title}/>)}
         </div>  
         <div className="lg:col-span-4 col-span-1">
             <div className="lg:sticky relative top-8">
@@ -26,4 +22,12 @@ export default function Home() {
       </div>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const posts = await (getPosts()) || [];
+
+  return {
+    props: { posts }
+  }
 }
