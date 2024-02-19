04858ee58dfd17d8c63e448ec1e75da46df2a936
diegocallegari96 <warsindustry@gmail.com>
2024-02-19 17:21:58 +0100

    widget out

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home({ posts }) {
         <div className="lg:col-span-8 col-span-1">
         {posts.slice().reverse().map((post) => <PostCard post={post.node} key={post.title}/>)}
         </div> 
-        <div data-gyg-href="https://widget.getyourguide.com/default/activities.frame" data-gyg-locale-code="en-US" data-gyg-widget="activities" data-gyg-number-of-items="3" data-gyg-partner-id="1SPD11R" data-gyg-q="amsterdam"></div> 
+        {/* <div data-gyg-href="https://widget.getyourguide.com/default/activities.frame" data-gyg-locale-code="en-US" data-gyg-widget="activities" data-gyg-number-of-items="3" data-gyg-partner-id="1SPD11R" data-gyg-q="amsterdam"></div>  */}
         <div className="lg:col-span-4 col-span-1">
             <div className="lg:sticky relative top-8">
               <PostWidget />
