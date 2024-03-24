259b67fb066b9213becbc1a802d83ea8cd7d8206
diegocallegari96 <warsindustry@gmail.com>
2024-03-24 17:16:59 +0100

    fixed related posts sorting on date

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -23,7 +23,7 @@ const PostWidget = ({ categories, slug}) => {
       <h3 className='text-xl mb-8 font-semibold border-b pb-4'>
           {slug ? "Related Posts" : "Recent Posts" }
       </h3>
-      {relatedPosts.map((post) => (
+      {relatedPosts.slice().reverse().map((post) => (
         <div key={post.title} className='flex items-center w-full mb-4'>
             <div className='w-16 flex-none'>
                 <img 
