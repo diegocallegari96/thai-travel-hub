57e5a6a391f8029805d7e557c7dd78394a1690b2
diegocallegari96 <warsindustry@gmail.com>
2024-03-31 17:56:44 +0200

    small adjust

--- components/Header.jsx
+++ components/Header.jsx
@@ -27,9 +27,9 @@ const Header = ({ post }) => {
     <div className='container mx-auto px-10 mb-8'>
         <Head post={post}>
         <title>{post?.title}</title>
+        <link rel='icon' href='../public/thaihubicowit.svg' />
                 <meta name='description' 
                       content={post?.description || 'Explore the beauty and culture of Thailand with ThaiTravelHub. Your ultimate guide to discovering hidden gems, delicious cuisine, and unforgettable experiences across the Land of Smiles.'}/>
-                <link rel='icon' href='../public/thaihubicowit.svg' />
                 {/* Open Graph */}
                 <meta property="og:title" 
                       content={post?.title} />
