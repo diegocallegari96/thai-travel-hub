cdcd7f1d1f6955bea5d0df8609e3ceb0ca3a3e08
diegocallegari96 <warsindustry@gmail.com>
2024-09-20 01:26:36 +0700

    klote bulletpoints

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -308,7 +308,8 @@ const PostDetail = ({ post }) => {
           </div>
         </div>
         <h1 className="mb-8 text-3xl font-semibold">{post.title}</h1>
-        {post.contentMarkdown => {
+        <h1 className="mb-8 text-3xl font-semibold">{post.title}</h1>
+        {post.content.raw.children.map((typeObj, index) => {
           const children = typeObj.children.map((item, itemIndex) =>
             getContentFragment(itemIndex, item.text, item)
           );
