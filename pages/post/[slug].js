502b048414e78f0fc716aae3aee092cde0e91b8c
diegocallegari96 <warsindustry@gmail.com>
2024-09-20 12:49:53 +0200

    mobile changes

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -6,52 +6,58 @@ import { useRouter } from 'next/router'
 const PostDetails = ({ post }) => {
   const router = useRouter();
 
-  if(router.isFallback) {
+  if (router.isFallback) {
     return <Loader />
   }
-  
+
   return (
-    <div className='container mx-auto px-10 mb-8'>
-        <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
-            <div className='col-span-1 lg:col-span-8'>
-                <PostDetail post={post} />
-                <Author author={post.author} />
-                <CommentsForm slug={post.slug} />
-                <Comments slug={post.slug} />
-            </div>
-            <div className='col-span-1 lg:col-span-4'>
-              <div className='relative lg:sticky top-8'>
-                <PostWidget slug={post.slug} categories={post.categories.map((category) => category.slug)} />
-                <Categories />
-                <div 
-                 data-gyg-href="https://widget.getyourguide.com/default/city.frame"
-                 data-gyg-location-id="169040" 
-                 data-gyg-locale-code="en-US" 
-                 data-gyg-widget="city"
-                 data-gyg-partner-id="1SPD11R">
-                 </div>
-              </div>
-            </div>
+    <div className="w-full mx-0 px-0 mb-8"> {/* Full width with no margin/padding */}
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        {/* Full width on mobile */}
+        <div className="col-span-1 lg:col-span-8 w-full px-0 lg:px-10"> {/* Ensure full width with responsive padding */}
+          <div className="w-full"> {/* Make each child component full width */}
+            <PostDetail post={post} className="w-full" />
+            <Author author={post.author} className="w-full" />
+            <CommentsForm slug={post.slug} className="w-full" />
+            <Comments slug={post.slug} className="w-full" />
+          </div>
         </div>
+        
+        {/* Sidebar widget */}
+        <div className="col-span-1 lg:col-span-4 w-full">
+          <div className="relative lg:sticky top-8">
+            <PostWidget slug={post.slug} categories={post.categories.map((category) => category.slug)} />
+            <Categories />
+            <div
+              data-gyg-href="https://widget.getyourguide.com/default/city.frame"
+              data-gyg-location-id="169040"
+              data-gyg-locale-code="en-US"
+              data-gyg-widget="city"
+              data-gyg-partner-id="1SPD11R"
+            >
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
 
 export default PostDetails
 
-export async function getStaticProps( {params} ) {
-    const data = await getPostDetails(params.slug);
-  
-    return {
-      props: { post: data }
-    }
+export async function getStaticProps({ params }) {
+  const data = await getPostDetails(params.slug);
+
+  return {
+    props: { post: data }
+  }
 }
 
 export async function getStaticPaths() {
-    const posts = await getPosts();
+  const posts = await getPosts();
 
-    return {
-        paths: posts.map(({ node: { slug } }) => ({ params: { slug } })),
-        fallback: true,
-    }
+  return {
+    paths: posts.map(({ node: { slug } }) => ({ params: { slug } })),
+    fallback: true,
+  }
 }
