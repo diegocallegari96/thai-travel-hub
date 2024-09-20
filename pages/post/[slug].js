dbf455d961869e55bbe7fe76ee0e72301f27df0f
diegocallegari96 <warsindustry@gmail.com>
2024-09-20 13:19:48 +0200

    changes

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -24,7 +24,7 @@ const PostDetails = ({ post }) => {
         </div>
         
         {/* Sidebar widget */}
-        <div className="col-span-1 lg:col-span-4 w-full">
+        <div className="col-span-1 lg:col-span-4 w-full lg:pr-10">
           <div className="relative lg:sticky top-8">
             <PostWidget slug={post.slug} categories={post.categories.map((category) => category.slug)} />
             <Categories />
