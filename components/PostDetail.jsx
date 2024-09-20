995ccce3675535d5e782f3859972684822e2f1fd
diegocallegari96 <warsindustry@gmail.com>
2024-09-20 09:25:20 +0200

    fixed double titles

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -308,7 +308,6 @@ const PostDetail = ({ post }) => {
           </div>
         </div>
         <h1 className="mb-8 text-3xl font-semibold">{post.title}</h1>
-        <h1 className="mb-8 text-3xl font-semibold">{post.title}</h1>
         {post.content.raw.children.map((typeObj, index) => {
           const children = typeObj.children.map((item, itemIndex) =>
             getContentFragment(itemIndex, item.text, item)
