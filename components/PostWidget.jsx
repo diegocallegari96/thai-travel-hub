948a5d75de1d3ecc03b03a0b3445faba6a794228
diegocallegari96 <warsindustry@gmail.com>
2024-09-25 10:04:32 +0200

    removed date featured/similar

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -33,9 +33,6 @@ const PostWidget = ({ categories, slug }) => {
             />
           </div>
           <div className="flex-grow ml-4">
-            <p className="text-gray-500 font-xs">
-              {moment(post.createdAt).format('MMM DD, YYYY')}
-            </p>
             <Link href={`/post/${post.slug}`} key={post.title} className="text-md">
               {post.title}
             </Link>
